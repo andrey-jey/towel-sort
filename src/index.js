c84987a8a0b2d6ce0fd573e2f8dddd1a90e9ee65
andrey-jey <kozlov13.an@mail.ru>
2020-03-19 17:52:22 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,17 @@
+module.exports = function towelSort (matrix) {
+
+  if (matrix == undefined || matrix.length<1) return []; 
+ 
+
+  let towel = [];
+  towel.push(matrix[0], matrix[1].reverse()); 
+
+
+  if (matrix[2]) towel.push(matrix[2]); 
+
+
+  if (matrix[3]) towel.push(matrix[3].reverse());
+
+
+  return towel.flat();
+}
